eaa5ab4caa4bc1cab6199daf7051f5b6eaeb1857
zhousz <13687646803@163.com>
2019-01-31 14:54:46 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,12 @@
+export default{
+    singular: true, // pages or page
+    routes: [{
+        path: '/',
+        component: './HelloWorld'
+    }],
+    plugins: [
+        ['umi-plugin-react', {
+
+        }]
+    ]
+};
